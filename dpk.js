981d28f72dfd948a7616ef9d90abcbf623f98f7d
Ifiok Udoidiok <ifiok@fleetrover.com>
2023-01-23 12:15:02 +0100

    Add comments to code

--- dpk.js
+++ dpk.js
@@ -1,10 +1,23 @@
 const crypto = require("crypto");
 const { DPK } = require("./constants");
 
+/**
+ * Utilizes the crypto module to hash input data in order to generate a deterministic partition key.
+ * @param {*} input 
+ * Computes the SHA3-512 hash digest of the input and @returns it as a hexadecimal string. 
+ * @returns 
+ */
 const hashInput = (input) => {
+  
   return crypto.createHash("sha3-512").update(input).digest("hex");
 };
-
+/**
+ * - Makes use of a constant to define the maximum length of the partition key.
+ * - Checks if the input is an event, and if so, will use the partition key from the event.
+ * - If the input is not an event, it will stringify the input and hash it to generate the partition key.
+ * @param {*} event 
+ * @returns candidate
+ */
 const getCandidate = (event) => {
   let candidate;
   if (event) {
@@ -32,6 +45,7 @@ exports.deterministicPartitionKey = (event) => {
     candidate = hashInput(candidate);
   }
 
+  // If the partition key exceeds the maximum length, it will be hashed again to ensure it is under the maximum length
   return candidate.length > DPK.MAX_PARTITION_KEY_LENGTH
     ? hashInput(candidate)
     : candidate;
